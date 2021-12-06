4506e26609af2aced0b61aebd8c097ef05797691
Stv018 <89112587+Stv018@users.noreply.github.com>
2021-12-06 12:01:14 +0800

    1206_2D-地圖
    使用TileMap+Composite Collider(合併碰撞器，TileMap專用)

---
+++ SaveFile_UsingDB_PP/Assets/Script/Char_2D.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ce1206
+{
+    /// <summary>
+    /// ¨¤¦â±±¨î¾¹
+    /// </summary>
+    public class Char_2D : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
